d2e5af403857ee25f527c2c36a669669fdfbccbe
Raia Abu Ahmad <raya.abu.ahmad@gmail.com>
2024-08-07 12:27:29 +0200

    added local executor version

--- examples/scilons_pipeline_local.py
+++ examples/scilons_pipeline_local.py
@@ -10,7 +10,7 @@ print("src_path:", src_path)
 sys.path[0] = src_path
 print("sys.path:", sys.path)
 
-from datatrove.executor.slurm import SlurmPipelineExecutor
+from datatrove.executor import LocalPipelineExecutor
 from datatrove.pipeline.dedup import MinhashDedupCluster, MinhashDedupFilter, MinhashDedupSignature
 from datatrove.pipeline.dedup.minhash import MinhashConfig, MinhashDedupBuckets
 from datatrove.pipeline.extractors import Trafilatura
@@ -39,9 +39,10 @@ for var in list(os.environ):
     if var.startswith('SLURM_'):
         del os.environ[var]
 
+
 def get_dataset_langs(dataset_name: str, hf_token: str) -> list:
     """
-    Function that gets a list of all available configs (i.e. langs) in the tmp4b dataset in order to 
+    Function that gets a list of all available configs (i.e. langs) in the tmp4b dataset in order to
     then download its own stopwords for the Gopher Quality Filtering.
     """
     try:
@@ -59,16 +60,17 @@ def get_dataset_langs(dataset_name: str, hf_token: str) -> list:
         else:
             raise e
 
+
 def get_filtered_stopwords(lang_list: list) -> dict:
     """
-    Takes list of available langs (i.e. configs) and returns a dict with lang codes as keys and lists of stopwords as values. 
+    Takes list of available langs (i.e. configs) and returns a dict with lang codes as keys and lists of stopwords as values.
     If no stopword list is found the list will be empty.
     """
 
     nltk.download('stopwords')
 
     stopwords_list = {lang: [] for lang in lang_list}
-    
+
     for lang in available_langs:
         try:
             stopwords_list[lang] = get_stopwords(lang)
@@ -93,7 +95,6 @@ def get_filtered_stopwords(lang_list: list) -> dict:
 
 
 def main():
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--token", type=str, help="Hugging Face API token")
     args = parser.parse_args()
@@ -101,9 +102,9 @@ def main():
     hf_token = args.token
 
     # Get all configs/langs in the tmp4b datasets
-    #available_langs = get_dataset_langs("malteos/tmp4b", hf_token)
+    # available_langs = get_dataset_langs("malteos/tmp4b", hf_token)
     available_langs = ['en']
-    #stopwords_list = get_filtered_stopwords(available_langs)
+    # stopwords_list = get_filtered_stopwords(available_langs)
     # Need to think about what happens when lang = unknown in terms of LanguageFilter and GopherQualityFilter.
 
     # iterate over all configs in tmp4b and apply filters
@@ -113,63 +114,59 @@ def main():
             MAIN_OUTPUT_PATH = "/netscratch/abu"
             FILTERING_OUTPUT_PATH = f"{MAIN_OUTPUT_PATH}/base_processing"
 
-            main_processing_executor = SlurmPipelineExecutor(
-            job_name=f"cc_{lang}",
-            pipeline=[
-                HuggingFaceDatasetReader(
-                    dataset="malteos/tmp4b",
-                    dataset_options={"token": hf_token, "name": lang}
-                ),
-                LanguageFilter(
-                    languages=lang, # need to make sure lang codes in tmp4b and in fasttext are same
-                    exclusion_writer=JsonlWriter(f"{FILTERING_OUTPUT_PATH}/2_non_english/",
-                    output_filename="${lang}/" + "/${rank}.jsonl.gz",)
-                    # folder structure: language/dump/file
+            main_processing_executor = LocalPipelineExecutor(
+                pipeline=[
+                    HuggingFaceDatasetReader(
+                        dataset="malteos/tmp4b",
+                        dataset_options={"token": hf_token, "name": lang}
                     ),
-                GopherRepetitionFilter(
-                    exclusion_writer=JsonlWriter(f"{FILTERING_OUTPUT_PATH}/removed/3_gopher_rep/{lang}")
+                    LanguageFilter(
+                        languages=lang,  # need to make sure lang codes in tmp4b and in fasttext are same
+                        exclusion_writer=JsonlWriter(f"{FILTERING_OUTPUT_PATH}/2_non_english/",
+                                                     output_filename="${lang}/" + "/${rank}.jsonl.gz", )
+                        # folder structure: language/dump/file
                     ),
-                GopherQualityFilter(
-                    language=lang,
-                    exclusion_writer=JsonlWriter(f"{FILTERING_OUTPUT_PATH}/removed/4_gopher_qual/{lang}")
+                    GopherRepetitionFilter(
+                        exclusion_writer=JsonlWriter(f"{FILTERING_OUTPUT_PATH}/removed/3_gopher_rep/{lang}")
                     ),
-                C4QualityFilter(
-                    filter_no_terminal_punct=False,
-                    exclusion_writer=JsonlWriter(f"{FILTERING_OUTPUT_PATH}/removed/5_c4/{lang}"),
+                    GopherQualityFilter(
+                        language=lang,
+                        exclusion_writer=JsonlWriter(f"{FILTERING_OUTPUT_PATH}/removed/4_gopher_qual/{lang}")
                     ),
-                FineWebQualityFilter(
-                    exclusion_writer=JsonlWriter(f"{FILTERING_OUTPUT_PATH}/removed/6_fineweb_qual/{lang}")
+                    C4QualityFilter(
+                        filter_no_terminal_punct=False,
+                        exclusion_writer=JsonlWriter(f"{FILTERING_OUTPUT_PATH}/removed/5_c4/{lang}"),
                     ),
-                JsonlWriter(f"{FILTERING_OUTPUT_PATH}/output/{lang}"),
+                    FineWebQualityFilter(
+                        exclusion_writer=JsonlWriter(f"{FILTERING_OUTPUT_PATH}/removed/6_fineweb_qual/{lang}")
+                    ),
+                    JsonlWriter(f"{FILTERING_OUTPUT_PATH}/output/{lang}"),
                 ],
-            tasks=10,
-            time="10:00:00",
-            logging_dir=f"{MAIN_OUTPUT_PATH}/logs/base_processing/{lang}",
-            slurm_logs_folder=f"{MAIN_OUTPUT_PATH}/logs/base_processing/{lang}/slurm_logs",  # must be local
-            randomize_start_duration=180,  # don't hit the bucket all at once with the list requests
-            mem_per_cpu_gb=2,
-            partition="RTXA6000",
+                tasks=10,
+                logging_dir=f"{MAIN_OUTPUT_PATH}/logs/base_processing/{lang}",
+                workers=5
             )
-        
+
         main_processing_executor.run()
 
-        # Minhash dedup process 
+        # Minhash dedup process
         # you can also change ngrams or the number of buckets and their size here
         minhash_config = MinhashConfig(
+            use_64bit_hashes=True,  # better precision -> fewer false positives (collisions)
             num_buckets=14,
             hashes_per_bucket=8,
             n_grams=5,
-            )
-            
+        )
+
         S3_MINHASH_BASE_PATH = f"{MAIN_OUTPUT_PATH}/minhash"
         S3_LOGS_FOLDER = f"{MAIN_OUTPUT_PATH}/logs/minhash"
         LOCAL_LOGS_FOLDER = "logs/minhash"
-        
+
         TOTAL_TASKS = 1000
-        
+
         # this is the original data that we want to deduplicate
         INPUT_READER = JsonlReader(f"{FILTERING_OUTPUT_PATH}/output/{lang}")  # this is the output from the first part
-        
+
         # stage 1 computes minhash signatures for each task (each task gets a set of files)
         stage1 = SlurmPipelineExecutor(
             job_name=f"mh1_{lang}",
@@ -177,7 +174,7 @@ def main():
                 INPUT_READER,
                 MinhashDedupSignature(
                     output_folder=f"{S3_MINHASH_BASE_PATH}/{lang}/signatures", config=minhash_config
-                    ),],
+                ), ],
             tasks=TOTAL_TASKS,
             time="5:00:00",
             partition="batch",
@@ -185,8 +182,8 @@ def main():
             slurm_logs_folder=f"{LOCAL_LOGS_FOLDER}/signatures/slurm_logs",
             randomize_start_duration=180,
             depends=main_processing_executor,  # only start after the first one completes
-            )
-            
+        )
+
         stage2 = SlurmPipelineExecutor(
             job_name=f"mh2_{lang}",
             pipeline=[
@@ -194,7 +191,7 @@ def main():
                     input_folder=f"{S3_MINHASH_BASE_PATH}/{lang}/signatures",
                     output_folder=f"{S3_MINHASH_BASE_PATH}/{lang}/buckets",
                     config=MinhashConfig(use_64bit_hashes=True),
-                    ),],
+                ), ],
             tasks=minhash_config.num_buckets * 50,  # the code supports parallelizing each bucket. here we run 50
             # workers per bucket
             randomize_start_duration=180,
@@ -204,8 +201,8 @@ def main():
             mem_per_cpu_gb=4,
             cpus_per_task=3,  # you can add run more (smaller) tasks if you do not have a lot of memory
             depends=stage1,
-            )
-            
+        )
+
         stage3 = SlurmPipelineExecutor(
             job_name=f"mh3_{lang}",
             pipeline=[
@@ -213,15 +210,15 @@ def main():
                     input_folder=f"{S3_MINHASH_BASE_PATH}/{lang}/buckets",
                     output_folder=f"{S3_MINHASH_BASE_PATH}/{lang}/remove_ids",
                     config=minhash_config,
-                    ),],
+                ), ],
             tasks=1,  # this step runs on a single task
             logging_dir=f"{S3_LOGS_FOLDER}/clustering",
             partition="batch",
             time="30:00:00",  # and can also be quite slow. Usually not this slow though
             mem_per_cpu_gb=25,
             cpus_per_task=8,  # if you dedup a full dump, you do need a lot of memory for this one
-            depends=stage2,)
-            
+            depends=stage2, )
+
         stage4 = SlurmPipelineExecutor(
             job_name=f"mh4_{lang}",
             pipeline=[
@@ -232,19 +229,18 @@ def main():
                 # run the PII removal
                 PIIFormatter(),
                 JsonlWriter(f"{S3_MINHASH_BASE_PATH}/{lang}/deduped_output"),
-                ],
+            ],
             tasks=TOTAL_TASKS,
             logging_dir=f"{S3_LOGS_FOLDER}/filtering",
             partition="batch",
             time="5:00:00",
             mem_per_cpu_gb=4,
             depends=stage3,
-            )
+        )
         # launch dedup pipelines
         stage4.run()
 
 
 # Classify 'unknown'?
 if __name__ == '__main__':
-
     main()
